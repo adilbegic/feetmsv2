05e8550f5c13f03af5f698bd265557df2925fe69
Adil <adil.begic@teletech.si>
2022-07-23 21:22:04 +0200

    10 - Create all the Packages (Models, Repos, Controllers and Services)

--- src/main/java/com/kindonthegenius/feetmsv2/ApplicationController.java
+++ src/main/java/com/kindonthegenius/feetmsv2/ApplicationController.java
@@ -11,8 +11,33 @@ public class ApplicationController {
         return "index";
     }
 
-    @GetMapping("/index2")
+    @GetMapping("/hr")
     public String widget(){
-        return "index2";
+        return "/hr/index";
+    }
+
+    @GetMapping("/fleet")
+    public String fleet(){
+        return "/fleet/index";
+    }
+
+    @GetMapping("/helpdesk")
+    public String helpdesk(){
+        return "/helpdesk/index";
+    }
+
+    @GetMapping("/accounts")
+    public String accounts(){
+        return "/accounts/index";
+    }
+
+    @GetMapping("/payroll")
+    public String payroll(){
+        return "/payroll/index";
+    }
+
+    @GetMapping("/parameters")
+    public String parameters(){
+        return "/parameters/index";
     }
 }
