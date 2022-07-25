3e4af4cab92e78caf6028fb60f5c2736cc437f5d
Adil <adil.begic@teletech.si>
2022-07-25 14:47:41 +0200

    edit and details country

--- src/main/java/com/kindonthegenius/feetmsv2/parameters/controllers/CountryController.java
+++ src/main/java/com/kindonthegenius/feetmsv2/parameters/controllers/CountryController.java
@@ -22,17 +22,30 @@ public class CountryController {
         return "parameters/countryList";
     }
 
-/*    @GetMapping("/countries")
-    @ResponseBody
-    public List<Country> getAll(Model model) {
-        List<Country> countries = countryService.getAll();
-
-        return countries;
-    }*/
 
     @GetMapping ("/countryAdd")
     public String addCountry() {
         return "parameters/countryAdd";
+    }
+
+    @GetMapping ("/countryEdit/{id}")
+    public String editCountry(@PathVariable Integer id, Model model) {
+        Country country = countryService.getById(id);
+        model.addAttribute("country", country);
+        return "parameters/countryEdit";
+    }
+
+    @GetMapping ("/countryDetails/{id}")
+    public String detailsCountry(@PathVariable Integer id, Model model) {
+        Country country = countryService.getById(id);
+        model.addAttribute("country", country);
+        return "parameters/countryDetails";
+    }
+
+    @GetMapping ("/countryEdit")
+    public String editCountry() {
+
+        return "parameters/countryEdit";
     }
 
     @PostMapping("/countries")
@@ -45,6 +58,12 @@ public class CountryController {
     @RequestMapping(value = "countries/delete/{id}", method = {RequestMethod.GET, RequestMethod.DELETE})
     public String delete(@PathVariable Integer id) {
         countryService.delete(id);
+        return "redirect:/countries";
+    }
+
+    @RequestMapping(value = "countries/update/{id}", method = {RequestMethod.GET, RequestMethod.PUT})
+    public String update(Country country) {
+        countryService.save(country);
         return "redirect:/countries";
     }
 }
